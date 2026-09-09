36297001ded8684d0269d063d57550d7f270b8a3
do.urrutia <do.urrutia@duocuc.cl>
2026-09-09 17:34:45 -0300

    Login MSAL con Redirect funcionando correctamente

---
+++ src/authConfig.js
@@ -0,0 +1,15 @@
+export const msalConfig = {
+    auth: {
+        clientId: "3666de5f-f393-474b-a2b8-e9140225e3af",
+        authority: "https://login.microsoftonline.com/43341e84-316b-40a7-a02f-5c94afd3d730/",
+        redirectUri: "http://localhost:5173",
+    },
+    cache: {
+        cacheLocation: "sessionStorage", 
+        storeAuthStateInCookie: false, 
+    }
+};
+
+export const loginRequest = {
+    scopes: ["User.Read"] 
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./authConfig";
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
+// En las versiones recientes de MSAL, es obligatorio inicializar la instancia antes de usarla
+msalInstance.initialize().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <MsalProvider instance={msalInstance}>
+        <App />
+      </MsalProvider>
+    </React.StrictMode>,
+  )
+});
