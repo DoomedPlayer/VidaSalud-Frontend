dcb82ffb4b9a28ef4a29e590233bd3c76847d399
do.urrutia <do.urrutia@duocuc.cl>
2026-09-17 09:50:03 -0300

    fix: rutas relativas en vite y url dinamica en MSAL para AWS

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,8 @@ export const msalConfig = {
     auth: {
         clientId: "3666de5f-f393-474b-a2b8-e9140225e3af",
         authority: "https://login.microsoftonline.com/43341e84-316b-40a7-a02f-5c94afd3d730/",
-        redirectUri: "http://localhost:5173",
+        // Cambiamos el localhost por window.location.origin para que sea dinámico
+        redirectUri: window.location.origin, 
     },
     cache: {
         cacheLocation: "sessionStorage", 
