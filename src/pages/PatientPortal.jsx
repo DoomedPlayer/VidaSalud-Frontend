255bd5af5e69346da9f9f6a4babe9bb2bbf3893f
bpalominosg <ben.palominos@duocuc.cl>
2026-09-18 18:34:35 -0300

    portal paciente cambio de horas de atencion

--- src/pages/PatientPortal.jsx
+++ src/pages/PatientPortal.jsx
@@ -22,7 +22,7 @@ export default function PatientPortal() {
     const [especialidad, setEspecialidad] = useState('');
     const [fechaReserva, setFechaReserva] = useState('');
     const [horaReserva, setHoraReserva] = useState('08:00');
-    const horariosFijos = ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30'];
+    const horariosFijos = ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00'];
 
     useEffect(() => {
         const fetchDatosIniciales = async () => {
